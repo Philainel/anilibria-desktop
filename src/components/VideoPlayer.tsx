3fc7ed8b73bd2fda7e6b1d4b39cc8303f5bcbaa1
Kraskaska <k.krasilnikov.2008@gmail.com>
2024-02-27 09:12:40 +0100

    simple overlay

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -3,5 +3,13 @@ import { getTitleHLS } from "../api";
 import { TitleT } from "../api/anilibria-types";
 
 export function VideoPlayer({title}: {title: TitleT}) {
-    return <ReactPlayer autoPlay={false} controls={false} url={getTitleHLS(title, title.player.list[0], "hd")} />
+    
+    return <div className="relative">
+        <ReactPlayer autoPlay={false} controls={false} url={getTitleHLS(title, title.player.list[0], "hd")} />
+        <div className="absolute top-0 left-0 w-full h-full z-[1]">
+            {/* overlay itself */}
+            <p> HELLO I AM OVERLAID!!</p>
+        </div>
+        {/* there is no className :skull: */}
+    </div>
 }
