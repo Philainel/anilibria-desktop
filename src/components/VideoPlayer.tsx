dc338e3fa2d1f29a3915c1c4090841ac5b8f9201
Kraskaska <k.krasilnikov.2008@gmail.com>
2024-02-27 14:18:13 +0100

    owo

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,14 +1,21 @@
 import ReactPlayer from "react-player";
 import { getTitleHLS } from "../api";
 import { TitleT } from "../api/anilibria-types";
+import { useState } from "react";
 
 export function VideoPlayer({title}: {title: TitleT}) {
-    
-    return <div className="relative">
-        <ReactPlayer autoPlay={false} controls={false} url={getTitleHLS(title, title.player.list[0], "hd")} />
-        <div className="absolute top-0 left-0 w-full h-full z-[1]">
+    const [isPlaying, setPlaying] = useState(false)
+    return <div className="relative w-fit">
+        <ReactPlayer autoPlay={false} controls={false} playing={isPlaying} url={getTitleHLS(title, title.player.list[0], "hd")} />
+        <div className="absolute top-0 left-0 w-full h-full z-[1] flex flex-col">
             {/* overlay itself */}
-            <p> HELLO I AM OVERLAID!!</p>
+            <div className="bg-gradient-to-b from-black to-transparent text-white p-4">
+                <p>{title.names.ru}</p>
+            </div>
+            <div className="flex-grow-[1]">{/* Pause Icon and stuff! */}</div>
+            <div className="bg-gradient-to-b from-transparent to-black text-white p-4">
+                <button onClick={() => setPlaying(!isPlaying)}>{isPlaying ? 'Pause' : 'Play'}</button>
+            </div>
         </div>
         {/* there is no className :skull: */}
     </div>
