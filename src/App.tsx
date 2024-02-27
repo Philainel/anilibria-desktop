81eb48fec8b18d6395814f1ee9375ff3d0787727
Kraskaska <k.krasilnikov.2008@gmail.com>
2024-02-27 15:41:52 +0100

    previous next play seek working

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { getRandomTitle } from "./api/getRandomTitle";
 import { getTitleHLS } from "./api";
 import { TitleT } from "./api/anilibria-types";
 import { VideoPlayer } from "./components/VideoPlayer";
+import { getTitle } from "./api/getTitle";
 
 // kraska was here owo :3
 export function App() {
     const [title, setTitle] = useState<TitleT>()
 	useEffect(() => {
-		getRandomTitle().then((title) => setTitle(title))
+		getRandomTitle(/* {code: 'tensei-shitara-slime-datta-ken'} */).then((title) => {console.log(title); setTitle(title)})
 	}, [])
 
     return <>
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,22 +1,45 @@
 import ReactPlayer from "react-player";
 import { getTitleHLS } from "../api";
 import { TitleT } from "../api/anilibria-types";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import * as Slider from '@radix-ui/react-slider';
 
-export function VideoPlayer({title}: {title: TitleT}) {
+export function VideoPlayer({ title }: { title: TitleT }) {
+    const playerRef = useRef<ReactPlayer>(null)
     const [isPlaying, setPlaying] = useState(false)
-    return <div className="relative w-fit">
-        <ReactPlayer autoPlay={false} controls={false} playing={isPlaying} url={getTitleHLS(title, title.player.list[0], "hd")} />
+    const [episode, setEpisode] = useState(0)
+    const [progress, _setProgress] = useState(0)
+    const setProgress = (progress: number) => {
+        playerRef.current?.seekTo(progress, "fraction")
+        _setProgress(progress)
+    }
+    return <div className="relative w-fit bg-black">
+        <ReactPlayer ref={playerRef} width={"100%"} height={"100%"} autoPlay={false} controls={false} playing={isPlaying} url={getTitleHLS(title, title.player.list[episode], "hd")} onProgress={({played}) => _setProgress(played)}/>
         <div className="absolute top-0 left-0 w-full h-full z-[1] flex flex-col">
             {/* overlay itself */}
-            <div className="bg-gradient-to-b from-black to-transparent text-white p-4">
+            <div className="bg-gradient-to-b from-black to-transparent text-white p-4 h-16">
                 <p>{title.names.ru}</p>
             </div>
-            <div className="flex-grow-[1]">{/* Pause Icon and stuff! */}</div>
-            <div className="bg-gradient-to-b from-transparent to-black text-white p-4">
-                <button onClick={() => setPlaying(!isPlaying)}>{isPlaying ? 'Pause' : 'Play'}</button>
+            <div className="flex-grow-[1]" onClick={() => setPlaying(!isPlaying)}>{/* Pause Icon and stuff! */}</div>
+            <div className="bg-gradient-to-b from-transparent to-black text-white flex flex-col items-center h-14 justify-end">
+                <Slider.Root className="h-3 w-[98%] rounded-full relative flex items-center group" max={1000} value={[progress * 1000]} onValueChange={value => setProgress(value[0] / 1000)}>
+                    <Slider.Track className="h-1 w-full rounded-full bg-gray-500 relative"> 
+                        <Slider.Range className="h-full rounded-full bg-white absolute" />
+                    </Slider.Track>
+                    <Slider.Thumb className="group-hover:w-2 group-hover:h-2 block rounded-full bg-white" />
+                </Slider.Root>
+                <div className="flex items-center gap-2 px-2 w-full">
+                    <button className={`w-8 h-8 flex items-center justify-center ${episode < 1 && 'text-gray-400'}`} onClick={() => setEpisode(episode - 1)} disabled={episode < 1}>
+                        <span className="material-symbols-outlined">skip_previous</span>
+                    </button>
+                    <button className="w-8 h-8 flex items-center justify-center" onClick={() => setPlaying(!isPlaying)} >
+                        {isPlaying ? <span className="material-symbols-outlined">pause</span> : <span className="material-symbols-outlined">play_arrow</span>}
+                    </button>
+                    <button className={`w-8 h-8 flex items-center justify-center ${episode >= title.player.list.length - 1 && 'text-gray-400'}`} onClick={() => setEpisode(episode + 1)} disabled={episode >= title.player.list.length - 1} >
+                        <span className="material-symbols-outlined">skip_next</span>
+                    </button>
+                </div>
             </div>
         </div>
-        {/* there is no className :skull: */}
     </div>
 }
