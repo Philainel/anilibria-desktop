f8304f27a18d58bb0b59aa8fe6c1f20d640e426b
Kraskaska <k.krasilnikov.2008@gmail.com>
2024-02-29 21:18:47 +0100

    tanstack setup

---
+++ src/routes/__root.tsx
@@ -0,0 +1,42 @@
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { useEffect, useState } from "react";
+import { TitleT } from "../api/anilibria-types";
+import { VideoPlayer } from "../components/VideoPlayer";
+import { getTitle } from "../api/getTitle";
+
+// kraska was here owo :3
+export function App() {
+    const [title, setTitle] = useState<TitleT>()
+    useEffect(() => {
+        getTitle({ code: 'tensei-shitara-slime-datta-ken' }).then((title) => { console.log(title); setTitle(title) })
+    }, [])
+
+    return <>
+        <header>
+            <nav>
+                <ul>
+                    <li>Релизы</li>
+                    <li>Каталог</li>
+                    <li>Избранное</li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <ul>
+                <li>реиз крупным планом</li>
+            </ul>
+            <ul>
+                <li>список недавних</li>
+                <li>список ожидаемых сегодня</li>
+
+            </ul>
+            <Outlet/>
+            <TanStackRouterDevtools/>
+        </main>
+    </>
+}
+
+export const rootRoute = createRootRoute({
+    component: App,
+})
