667745c4d9aeabc7bb007b7d47306ec107973284
Kraskaska <k.krasilnikov.2008@gmail.com>
2024-03-01 19:30:39 +0100

    episode list

--- src/routes/_navbar/title/$code.tsx
+++ src/routes/_navbar/title/$code.tsx
@@ -10,10 +10,17 @@ function TitleInformation() {
             <h2 className="text-3xl mb-4">{title.names.ru}</h2>
             <p className="my-4 text-xl">{title.description}</p>
             <div className='my-4 mt-auto flex gap-4'>
-                <Link to="/player/$code/$episode" params={{code: title.code, episode: "1"}} className='bg-brand-primary text-brand-light px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>play_arrow</MDIcon>Смотреть</Link>
+                <Link to="/player/$code/$episode" params={{ code: title.code, episode: "1" }} className='bg-brand-primary text-brand-light px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>play_arrow</MDIcon>Смотреть</Link>
                 <a className='bg-brand-light text-brand-dark px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>schedule</MDIcon>Отложить</a>
                 <a className='bg-brand-light text-brand-dark px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>star</MDIcon>Избранное</a>
             </div>
+            <h3 className="my-4 text-2xl">Список эпизодов</h3>
+            <ul>
+                {title.player.list.map(it => <li key={it.uuid} className='text-brand-primary underline'><Link to="/player/$code/$episode" params={{code: title.code, episode: `${it.episode}`}}>{it.name == null ? "Эпизод " : "E"}{it.episode.toLocaleString('en-US', {
+                    minimumIntegerDigits: 2,
+                    useGrouping: false
+                })}{it.name != null && ` - ${it.name}`}</Link></li>)}
+            </ul>
         </div>
     </main>
 }
