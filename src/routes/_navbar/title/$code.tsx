5e711768057cca56ec7bf583ead3ae0d648d8f38
Kraskaska <k.krasilnikov.2008@gmail.com>
2024-03-02 12:58:03 +0100

    update title page to use new title component

--- src/routes/_navbar/title/$code.tsx
+++ src/routes/_navbar/title/$code.tsx
@@ -2,12 +2,13 @@ import { Link, createFileRoute } from '@tanstack/react-router'
 import getTitle from '../../../api/getTitle'
 import { MDIcon } from '../../../components/MDIcon'
 import { TitleT } from '../../../api/anilibria-types'
+import Title from '../../../components/title'
 
 // TODO: POSTER DOMAIN
-export function TitleInformation({Route}: {Route: any}) {
+export function TitleInformation({ Route }: { Route: any }) {
     const title: TitleT = Route.useLoaderData()
-    return <main className='p-4 flex gap-4'>
-        <img src={"https://wwnd.anilib.moe/" + title.posters.original.url} alt={`Постер для "${title.names.ru}"`} className="rounded-3xl h-[16rem]" />
+    return <main className='p-4 flex flex-col gap-4'>
+        {/* <img src={"https://wwnd.anilib.moe/" + title.posters.original.url} alt={`Постер для "${title.names.ru}"`} className="rounded-3xl h-[16rem]" />
         <div>
             <h2 className="text-3xl mb-4">{title.names.ru}</h2>
             <p className="my-4 text-xl">{title.description}</p>
@@ -16,18 +17,19 @@ export function TitleInformation({Route}: {Route: any}) {
                 <a className='bg-brand-light text-brand-dark px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>schedule</MDIcon>Отложить</a>
                 <a className='bg-brand-light text-brand-dark px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>star</MDIcon>Избранное</a>
             </div>
-            <h3 className="my-4 text-2xl">Список эпизодов</h3>
-            <ul>
-                {title.player.list.map(it => <li key={it.uuid} className='text-brand-primary underline'><Link to="/player/$code/$episode" params={{code: title.code, episode: `${it.episode}`}}>{it.name == null ? "Эпизод " : "E"}{it.episode.toLocaleString('en-US', {
-                    minimumIntegerDigits: 2,
-                    useGrouping: false
-                })}{it.name != null && ` - ${it.name}`}</Link></li>)}
-            </ul>
-        </div>
+        </div> */}
+        <Title suspendQuery={() => title} />
+        <h3 className="my-4 text-2xl">Список эпизодов</h3>
+        <ul>
+            {title.player.list.map(it => <li key={it.uuid} className='text-brand-primary underline'><Link to="/player/$code/$episode" params={{ code: title.code, episode: `${it.episode}` }}>{it.name == null ? "Эпизод " : "E"}{it.episode.toLocaleString('en-US', {
+                minimumIntegerDigits: 2,
+                useGrouping: false
+            })}{it.name != null && ` - ${it.name}`}</Link></li>)}
+        </ul>
     </main>
 }
 
 export const Route = createFileRoute('/_navbar/title/$code')({
-    component: () => <TitleInformation Route={Route}/>,
+    component: () => <TitleInformation Route={Route} />,
     loader: ({ params }) => getTitle({ code: params.code })
 })
