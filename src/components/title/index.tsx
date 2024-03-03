80dc54e1cd405aa07531e42121f884858664ac30
Kraskaska <k.krasilnikov.2008@gmail.com>
2024-03-03 11:07:07 +0100

    suspending posters

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -4,7 +4,7 @@ import TitleData from "./title";
 import { TitleT } from "../../api/anilibria-types";
 
 export default function Title({ suspendQuery, key, allowExpanding, short = false }: { suspendQuery: () => TitleT | Promise<TitleT>, key?: string, allowExpanding?: boolean, short?: boolean }) {
-	return <Suspense fallback={TitleFallback({short})}>
+	return <Suspense fallback={TitleFallback({short})} key={`suspense-${key}`}>
 		<TitleData suspendQuery={suspendQuery} allowExpanding={allowExpanding} short={short} key={key} />
 	</Suspense>
 }
--- src/components/title/rawTitle.tsx
+++ src/components/title/rawTitle.tsx
@@ -1,17 +1,20 @@
 import { Link } from "@tanstack/react-router";
 import { MDIcon } from "../MDIcon";
 import { TitleT } from "../../api/anilibria-types";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { useAppSelector } from "../../store";
 import { getEpisodeProgress, getTitleProgress } from "../../store/slice/watchProgress";
+import { Img } from "react-suspense-img";
 
-export function RawTitleData({ title, short=false, allowExpanding, key }: { title: TitleT; short?: boolean; allowExpanding?: boolean | undefined; key?: any }) {
+export function RawTitleData({ title, short = false, allowExpanding, key }: { title: TitleT; short?: boolean; allowExpanding?: boolean | undefined; key?: any }) {
 	const lastEp = useAppSelector(getTitleProgress(title.code));
 	const episode = lastEp == -1 ? undefined : lastEp == title.player?.episodes?.last ? "last" : lastEp;
 	const epProgress = useAppSelector(getEpisodeProgress(title.code, lastEp));
 	const [expanded, setExpanded] = useState(false);
 	return <section className='flex items-center' key={key}>
-		<img className='block bg-brand-light text-brand-dark aspect-[2/3] rounded-md h-fit' width={short ? 210 : 256} height={short ? 240 : 384} src={`https://wwnd.anilib.moe${title.posters.medium.url}`} />
+		<Suspense fallback={<div className='block bg-brand-semidark aspect-[2/3] rounded-md h-fit animate-pulse' style={{ width: short ? 210 : 256, height: short ? 240 : 384 }} />}>
+			<Img className='block text-brand-dark aspect-[2/3] rounded-md h-fit' width={short ? 210 : 256} height={short ? 240 : 384} src={`https://wwnd.anilib.moe${title.posters.medium.url}`} />
+		</Suspense>
 		<div className='bg-transparent text-brand-light p-8 flex flex-col gap-6 justify-stretch w-full'>
 			<Link to="/title/$code" params={{ code: title.code }}><h2 className='text-3xl font-bold'>{title.names.ru}</h2></Link>
 			<div className="flex flex-row justify-start gap-10 md:gap-16">
@@ -51,7 +54,7 @@ export function RawTitleData({ title, short=false, allowExpanding, key }: { titl
 				{allowExpanding && title?.description?.length > 250 && <button className="text-left text-blue-500 h-8" onClick={() => setExpanded(!expanded)}>{expanded ? 'Свернуть' : 'Подробнее...'}</button>}
 			</div>
 			<div className='my-4 mt-auto flex'>
-				<Link to="/player/$code/$episode" params={{ code: title.code, episode: `${episode == "last" ? title.player.episodes.last : episode ?? 1}` }} search={{progress: epProgress}} className='bg-brand-primary text-brand-light px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>play_arrow</MDIcon>{episode == undefined ? 'Начать просмотр' : episode == "last" ? 'Посмотреть заново' : `Продолжить с серии ${episode}`}</Link>
+				<Link to="/player/$code/$episode" params={{ code: title.code, episode: `${episode == "last" ? title.player.episodes.last : episode ?? 1}` }} search={{ progress: epProgress }} className='bg-brand-primary text-brand-light px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>play_arrow</MDIcon>{episode == undefined ? 'Начать просмотр' : episode == "last" ? 'Посмотреть заново' : `Продолжить с серии ${episode}`}</Link>
 				{short && <Link to="/title/$code" params={{ code: title.code }} className='bg-brand-dark text-brand-light px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>list_alt</MDIcon>{title.type.string == "MOVIE" ? 'Подробнее' : 'Эпизоды'}</Link>}
 				<a className='bg-brand-dark text-brand-light px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>schedule</MDIcon>Отложить</a>
 				<a className='bg-brand-dark text-brand-light px-4 py-2 rounded-md flex items-center w-fit gap-2 cursor-pointer'><MDIcon className='inline-block'>star</MDIcon>Избранное</a>
