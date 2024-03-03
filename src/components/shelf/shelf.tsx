8c623f2b73c7c43b98be856f48c2af4a1091f44e
Kraskaska <k.krasilnikov.2008@gmail.com>
2024-03-03 10:58:57 +0100

    shelf titles are now width unbounded

--- src/components/shelf/shelf.tsx
+++ src/components/shelf/shelf.tsx
@@ -8,7 +8,7 @@ export function ShelfData({ name, children: queryFn, key = name }: { name: strin
     return <section className='flex flex-col'>
         <h2 className='text-3xl my-4'>{name}</h2>
         <div className="flex gap-4 overflow-x-auto">
-            {titles.map(it => <Link to="/title/$code" params={{code: it.code}} key={it.code}><img className='bg-brand-light text-brand-dark aspect-[2/3] rounded-md h-max' width={256} height={384} src={`https://wwnd.anilib.moe${it.posters.medium.url}`} /></Link>)}
+            {titles.map(it => <Link to="/title/$code" params={{code: it.code}} key={it.code}><img className='bg-brand-light text-brand-dark aspect-[2/3] rounded-md max-w-[unset]' width={256} height={384} src={`https://wwnd.anilib.moe${it.posters.medium.url}`} /></Link>)}
         </div>
     </section>;
 }
